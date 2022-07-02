4ba1c253aadfdfc77bf1e7c148d267ac3e2f9018
CharltonGilbert <gilbertcharlton11@gmail.com>
2022-07-02 20:48:24 +0700

    update

--- project/lib/Mainpage.dart
+++ project/lib/Mainpage.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:project/dataclass.dart';
 import 'package:project/filter.dart';
 import 'package:project/listbarang.dart';
 import 'dbservices.dart';
@@ -21,6 +22,7 @@ class _homePageState extends State<homePage> {
   int _selectedIndex = 0;
   String name = " ";
   final user = FirebaseAuth.instance.currentUser!;
+  final search = TextEditingController();
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
@@ -63,15 +65,20 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Halo, " + user.email.toString()),centerTitle: true,
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: Icon(Icons.exit_to_app),
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
-        ),
-      ),
+          title: Card(
+        child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              border: InputBorder.none,
+              hintText: 'Search nama...',
+            ),
+            onChanged: (val) {
+              setState(() {
+                name = val;
+              });
+            }), 
+            
+      )),
       //create a button on top of the screen
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -83,48 +90,54 @@ class _homePageState extends State<homePage> {
         child: Icon(Icons.search),
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: Database.getData(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text("error");
-            } else if (snapshot.hasData || snapshot.data != null) {
-              return ListView.separated(
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot dsStok = snapshot.data!.docs[index];
-                    String lvnamabarang = dsStok["namabarang"];
-                    String lvkategori = dsStok["kategori"];
-                    String lvjumlah = dsStok["jumlah"];
-                    return Card(
-                      child: ListTile(
-                        title: Text(lvnamabarang),
-                        subtitle: Text(lvkategori),
-                        trailing: Text(lvjumlah),
-                        onTap: () {
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //       builder: (context) => narikbarang()),
-                          // );
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => narikbarang(
-                                  namabarang: lvnamabarang,
-                                  kategori: lvkategori,
-                                  jumlah: lvjumlah)));
-                        },
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => SizedBox(height: 8.0),
-                  itemCount: snapshot.data!.docs.length);
-            }
-            return Center(
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  Colors.pinkAccent,
-                ),
-              ),
-            );
-          }),
+              stream: (name != " " && name != null)
+              ? FirebaseFirestore.instance
+                  .collection('tbStok')
+                  .where('namabarang', isEqualTo: name)
+                  .snapshots():
+              FirebaseFirestore.instance.collection("tbStok").snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text("error");
+                } else if (snapshot.hasData || snapshot.data != null) {
+                  return ListView.separated(
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot dsStok = snapshot.data!.docs[index];
+                        String lvnamabarang = dsStok["namabarang"];
+                        String lvkategori = dsStok["kategori"];
+                        String lvjumlah = dsStok["jumlah"];
+                        return Card(
+                          child: ListTile(
+                            title: Text(lvnamabarang),
+                            subtitle: Text(lvkategori),
+                            trailing: Text(lvjumlah),
+                            onTap: () {
+                              // Navigator.push(
+                              //   context,
+                              //   MaterialPageRoute(
+                              //       builder: (context) => narikbarang()),
+                              // );
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => narikbarang(
+                                      namabarang: lvnamabarang,
+                                      kategori: lvkategori,
+                                      jumlah: lvjumlah)));
+                            },
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) => SizedBox(height: 8.0),
+                      itemCount: snapshot.data!.docs.length);
+                }
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Colors.pinkAccent,
+                    ),
+                  ),
+                );
+              }),
+            
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
